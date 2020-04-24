27a796152472299aabe5a1059bdde63b84ce61a5
Antoine Salesse <antoinesalesse@gmail.com>
2020-04-24 16:53:46 +0200

    quite clean project

--- server/grails-app/controllers/fr/younup/helloworldcicd/HelloWorldController.groovy
+++ server/grails-app/controllers/fr/younup/helloworldcicd/HelloWorldController.groovy
@@ -1,8 +1,9 @@
 package fr.younup.helloworldcicd
 
-
+import grails.compiler.GrailsCompileStatic
 import grails.rest.RestfulController
 
+@GrailsCompileStatic
 class HelloWorldController extends RestfulController {
     static responseFormats = ['json']
 
