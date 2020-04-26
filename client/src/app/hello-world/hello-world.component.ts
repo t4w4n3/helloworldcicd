66fdab05b091df277b018316db53859a90f5235a
Antoine Salesse <antoinesalesse@gmail.com>
2020-04-26 17:39:53 +0200

    backend and front-end version

--- client/src/app/hello-world/hello-world.component.ts
+++ client/src/app/hello-world/hello-world.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {version} from "package.json";
 
 @Component({
   selector: 'app-hello-world',
@@ -14,12 +15,23 @@ export class HelloWorldComponent implements OnInit {
 
   ngOnInit() {
     this.getMessage();
+    this.getBackEndInfo();
   }
 
   message: string;
+  backEndVersion: string;
+  frontEndVersion: string = version
 
   getMessage() {
     this.getConfigMessage().subscribe(data => this.message = data['message'])
+  }
+
+  getBackEndInfo() {
+    this.getConfigBackEndInfo().subscribe(data => this.backEndVersion = data['appversion']);
+  }
+
+  getConfigBackEndInfo() {
+    return this.http.get('http://localhost:8080/');
   }
 
   getConfigMessage() {
